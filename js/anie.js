1055be6347fa5552f953a0fe0e6ad1f0e24b5aa5
Gabriel Masson <gabrielmasson.a3912@gmail.com>
2016-11-27 19:02:54 -0200

    update

---
+++ js/anie.js
@@ -0,0 +1,12 @@
+/*
+  Anie 1.1.0
+  https://github.com/gmasson/anie
+  License MIT
+*/
+
+function block(id){ document.getElementById(id).style.display="block"; }
+function none(id){ document.getElementById(id).style.display="none"; }
+function print() { window.print() }
+function back() { window.history.go(-1) }
+function next() { window.history.go(1) }
+function reload() { window.history.go(0) }
